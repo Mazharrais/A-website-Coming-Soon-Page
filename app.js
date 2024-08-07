b6be86a284682de5317d3c515bd55cc8a7882b45
Mazharrais <mazharmajo72@gmail.com>
2024-08-07 14:38:53 +0500

    add code

--- app.js
+++ app.js
@@ -5,15 +5,19 @@ var x = setInterval(function (){
   var now = new Date().getTime();
   var distance = countDownDate - now;
 
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60 ));
-  var mintues = Math.floor((distance % (1000 * 60 * 60 )) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
 
   
   document.getElementById("days").innerHTML = days;
   document.getElementById("hours").innerHTML = hours;
-  document.getElementById("mmintues").innerHTML = mintues;
+  document.getElementById("mintues").innerHTML = minutes;
   document.getElementById("seconds").innerHTML = seconds;
+
+
+  
 
 },1000)
