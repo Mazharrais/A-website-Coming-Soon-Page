d43c21eb335eb92ec910aa97ca0d034f47985884
Mazharrais <mazharmajo72@gmail.com>
2024-08-07 13:47:27 +0500

    add code

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 
-var countDownDate = new Date ("Dec 30, 2023 00:00:00").getTime();
+var countDownDate = new Date ("Dec 30, 2021 00:00:00").getTime();
 var x = setInterval(function (){
   var now = new Date().getTime();
   var distance = countDownDate - now;
@@ -9,4 +9,11 @@ var x = setInterval(function (){
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60 ));
   var mintues = Math.floor((distance % (1000 * 60 * 60 )) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("mmintues").innerHTML = mintues;
+  document.getElementById("seconds").innerHTML = seconds;
+
 },1000)
